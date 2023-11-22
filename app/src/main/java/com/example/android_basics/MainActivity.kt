c5f14c66c5c76a07fa4b6380e07d5ec2de97b882
mkaychuks <sachufusi@gmail.com>
2023-11-22 13:43:07 +0100

    Added a night mode qualifier to my images

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,7 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Android_basicsTheme {
                 // A surface container using the 'background' color from the theme
-               Image(painter = painterResource(id = R.drawable.image), contentDescription = null)
+               Image(painter = painterResource(id = R.drawable.image),
+                   contentDescription = null,
+                   modifier = Modifier.fillMaxSize(),
+                   )
             }
         }
     }
