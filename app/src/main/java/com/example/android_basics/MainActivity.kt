0bc21c9ad010ef99e8a7bc1cd12321db27c2e67c
mkaychuks <sachufusi@gmail.com>
2023-11-22 14:10:37 +0100

    sending an intent to another activity within our app

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.android_basics
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.android_basics.ui.theme.Android_basicsTheme
 
@@ -20,11 +20,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Android_basicsTheme {
-                // A surface container using the 'background' color from the theme
-               Image(painter = painterResource(id = R.drawable.image),
-                   contentDescription = null,
+               Column (
                    modifier = Modifier.fillMaxSize(),
-                   )
+                   verticalArrangement = Arrangement.Center,
+                   horizontalAlignment = Alignment.CenterHorizontally
+               ){
+                   Button(onClick = {
+                       Intent(applicationContext, SecondActivity::class.java).also {
+                           startActivity(it)
+                       }
+                   }) {
+                       Text(text = "Click Me")
+                   }
+               }
+
             }
         }
     }
