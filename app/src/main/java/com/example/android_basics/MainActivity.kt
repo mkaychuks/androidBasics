3364b99d0534e13c2057e86e0d6c8730f166d57f
mkaychuks <sachufusi@gmail.com>
2023-11-22 13:34:28 +0100

    Basics of adding an image

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -3,31 +3,24 @@ package com.example.android_basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.android_basics.ui.theme.Android_basicsTheme
 
 class MainActivity : ComponentActivity() {
-
-    private  val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Android_basicsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = viewModel.backgroundColor) {
-                   Button(onClick = {
-                       viewModel.changeBackgroundColor()
-                   }) {
-                       Text(text = "Change Background")
-                   }
-                }
+               Image(painter = painterResource(id = R.drawable.image), contentDescription = null)
             }
         }
     }
