adf6f90e8900a008c09a1e545db2987bd4b63e4f
mkaychuks <sachufusi@gmail.com>
2023-11-23 13:52:53 +0100

    Dynamic broadcast receiver

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -1,27 +1,25 @@
 package com.example.android_basics
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.android_basics.ui.theme.Android_basicsTheme
 
 class MainActivity : ComponentActivity() {
+    private val airplaneModeReceiver: AirplaneModeReceiver = AirplaneModeReceiver()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        registerReceiver(airplaneModeReceiver, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
         setContent {
             Android_basicsTheme {
                Column (
@@ -29,38 +27,16 @@ class MainActivity : ComponentActivity() {
                    verticalArrangement = Arrangement.Center,
                    horizontalAlignment = Alignment.CenterHorizontally
                ){
-                   Button(onClick = {
-//                        OPENING AN EXTERNAL APP, EXPLICIT INTENT
-                       Intent(Intent.ACTION_MAIN).also {
-                           it.`package` = "com.google.android.youtube"
-                           try {
-                               startActivity(it)
-                           } catch (e: ActivityNotFoundException){
-                               println(e)
-                           }
-                       }
-                   }) {
-                       Text(text = "Open Youtube")
-                   }
-                   Spacer(modifier = Modifier.height(30.dp))
-                   Button(onClick = {
-                       // IMPLICIT INTENT
-                       val intent = Intent(Intent.ACTION_SEND).apply{
-                           type = "text/plain"
-                           putExtra(Intent.EXTRA_TEXT, "This is the body of the mail")
-                       }
-                       if(intent.resolveActivity(packageManager) != null){
-                           startActivity(intent)
-                       }
 
-                       Intent()
-                   }) {
-                       Text(text = "Send a message")
-                   }
                }
 
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(airplaneModeReceiver)
     }
 }
 
