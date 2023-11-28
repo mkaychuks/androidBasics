400b67d1cbf469818bdbf89a852dd77164cfbaa3
mkaychuks <sachufusi@gmail.com>
2023-11-28 11:23:17 +0100

    working with images and the Box

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.android_basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,7 +32,7 @@ class MainActivity : ComponentActivity() {
                    modifier = Modifier.fillMaxSize(),
                    color = MaterialTheme.colorScheme.background
                ) {
-                   GreetingText(message = "Happy Birthday", from = "from Emma")
+                   GreetingImage(message = "Happy Birthday", from = "from Emma")
                }
             }
         }
@@ -52,17 +55,36 @@ fun GreetingText(modifier: Modifier = Modifier, message: String, from: String){
         Text(
             text = from,
             fontSize = 30.sp,
-            modifier = modifier.padding(16.dp)
+            modifier = modifier
+                .padding(16.dp)
                 .align(alignment = Alignment.End),
 
         )
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = "background photo"
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = modifier.fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Android_basicsTheme {
-        GreetingText(message = "Happy Birthday Sam", from = "from Emma")
+        GreetingImage(message = "Happy Birthday Sam", from = "from Emma")
     }
 }
