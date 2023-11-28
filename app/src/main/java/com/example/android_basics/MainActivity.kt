40d9d616dc263728c37f1b60f7a817456482b1b1
mkaychuks <sachufusi@gmail.com>
2023-11-28 11:02:39 +0100

    my first compose app

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.android_basics.ui.theme.Android_basicsTheme
 
@@ -19,12 +25,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Android_basicsTheme {
-               Column (
+               Surface(
                    modifier = Modifier.fillMaxSize(),
-                   verticalArrangement = Arrangement.Center,
-                   horizontalAlignment = Alignment.CenterHorizontally
-               ){
-
+                   color = MaterialTheme.colorScheme.background
+               ) {
+                   GreetingText(message = "Happy Birthday", from = "from Emma")
                }
             }
         }
@@ -33,14 +38,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(modifier: Modifier = Modifier, message: String, from: String){
-    Text(
-        text = message,
-        fontSize = 24.sp,
-    )
-    Text(
-        text = from,
-        fontSize = 14.sp,
-    )
+    Column(
+        modifier = modifier
+            .padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 100.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 30.sp,
+            modifier = modifier.padding(16.dp)
+                .align(alignment = Alignment.End),
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
