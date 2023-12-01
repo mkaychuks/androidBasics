a27f4406bbe45121ce11332743b8d203b0e9313d
mkaychuks <sachufusi@gmail.com>
2023-12-01 12:08:31 +0100

    Re-do of the lemonade app on my own

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -5,24 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,10 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.android_basics.ui.theme.Android_basicsTheme
@@ -48,42 +36,84 @@ class MainActivity : ComponentActivity() {
                    modifier = Modifier.fillMaxSize(),
                    color = MaterialTheme.colorScheme.background
                ) {
-                    DiceRoller()
+                   LemonadeApp()
                }
             }
         }
     }
 }
 
+
 @Composable
-fun DiceRoller() {
-    var sideUp by remember {
+fun LemonadeApp() {
+    var currentIndex by remember {
         mutableStateOf(1)
     }
-   Column(
-       modifier = Modifier
-           .fillMaxSize()
-           .background(Color.White),
-       horizontalAlignment = Alignment.CenterHorizontally,
-       verticalArrangement = Arrangement.Center
-   ) {
-       val diceImage = when(sideUp){
-           1 -> painterResource(id = R.drawable.dice_1)
-           2 -> painterResource(id = R.drawable.dice_2)
-           3 -> painterResource(id = R.drawable.dice_3)
-           4 -> painterResource(id = R.drawable.dice_4)
-           5 -> painterResource(id = R.drawable.dice_5)
-           else -> painterResource(id = R.drawable.dice_6)
-       }
-       Image(painter = diceImage, contentDescription = null)
-       Button(
-           onClick = {
-               sideUp = (1..6).random()
-           }
-       ) {
-           Text(text = "Roll the Dice")
-       }
-   } 
+
+    var squeezeIndex by remember {
+        mutableStateOf(0)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val image = when(currentIndex){
+            1 -> painterResource(id = R.drawable.lemon_tree)
+            2 -> painterResource(id = R.drawable.lemon_squeeze)
+            3 -> painterResource(id = R.drawable.lemon_drink)
+            else -> painterResource(id = R.drawable.lemon_restart)
+        }
+        val description: String = when(currentIndex){
+            1 -> "Tap the lemon tree to select a lemon"
+            2 -> "Keep tapping the lemon to squeeze it"
+            3 -> "Tap the lemonade to drink it"
+            else -> "Tap the empty glass to start again"
+        }
+
+        when(currentIndex){
+            2 -> Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                    modifier = Modifier.clickable {
+                        squeezeIndex -= 1
+                        if(squeezeIndex == 0) currentIndex += 1
+                    }
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(
+                    text = description,
+                    color = Color.Black
+                )
+            }
+            else -> Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                    modifier = Modifier.clickable {
+                        currentIndex += 1
+                        squeezeIndex = (2..4).random()
+                        if(currentIndex == 5) currentIndex = 1
+                    }
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(
+                    text = description,
+                    color = Color.Black
+                )
+            }
+        }
+
+    }
 }
 
 
@@ -92,6 +122,6 @@ fun DiceRoller() {
 @Composable
 fun LemonadeAppPreview() {
     Android_basicsTheme {
-        DiceRoller()
+        LemonadeApp()
     }
 }
