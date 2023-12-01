abb2c348c7e2092f72f27aba72a385f67a441cf7
mkaychuks <sachufusi@gmail.com>
2023-12-01 15:27:00 +0100

    Tip calculator unused imports cleared

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.android_basics
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -27,8 +23,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
