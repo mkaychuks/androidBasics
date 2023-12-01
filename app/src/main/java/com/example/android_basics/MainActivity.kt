95746f3f6040b111b25c506a885797dfd2f5ddfe
mkaychuks <sachufusi@gmail.com>
2023-12-01 11:13:03 +0100

    Re-do of the dice roller on my own

--- app/src/main/java/com/example/android_basics/MainActivity.kt
+++ app/src/main/java/com/example/android_basics/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -47,142 +48,50 @@ class MainActivity : ComponentActivity() {
                    modifier = Modifier.fillMaxSize(),
                    color = MaterialTheme.colorScheme.background
                ) {
-                   LemonadeApp()
+                    DiceRoller()
                }
             }
         }
     }
 }
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LemonadeApp() {
-
-    var currentStep by remember { mutableStateOf(1) }
-
-    var squeezeCount by remember { mutableStateOf(0) }
-
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        text = "Lemonade",
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                colors = TopAppBarDefaults.smallTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-            )
-        }
-    ) { innerPadding ->
-        Surface(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .background(MaterialTheme.colorScheme.tertiaryContainer),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            when (currentStep) {
-                1 -> {
-                    LemonTextAndImage(
-                        textLabelResourceId = R.string.lemon_tree,
-                        drawableResourceId = R.drawable.lemon_tree,
-                        contentDescriptionResourceId = R.string.lemon_tree_description,
-                        onImageClick = {
-                            currentStep = 2
-                            squeezeCount = (2..4).random()
-                        }
-                    )
-                }
-                2 -> {
-                    LemonTextAndImage(
-                        textLabelResourceId = R.string.squeeze_lemon,
-                        drawableResourceId = R.drawable.lemon_squeeze,
-                        contentDescriptionResourceId = R.string.lemon_description,
-                        onImageClick = {
-                            squeezeCount--
-                            if (squeezeCount == 0) {
-                                currentStep = 3
-                            }
-                        }
-                    )
-                }
-
-                3 -> {
-                    LemonTextAndImage(
-                        textLabelResourceId = R.string.drink_lemon,
-                        drawableResourceId = R.drawable.lemon_drink,
-                        contentDescriptionResourceId = R.string.glass_of_lemonade,
-                        onImageClick = {
-                            currentStep = 4
-                        }
-                    )
-                }
-                4 -> {
-                    LemonTextAndImage(
-                        textLabelResourceId = R.string.empty_glass,
-                        drawableResourceId = R.drawable.lemon_restart,
-                        contentDescriptionResourceId = R.string.empty_glass_desc,
-                        onImageClick = {
-                            currentStep = 1
-                        }
-                    )
-                }
-            }
-        }
+fun DiceRoller() {
+    var sideUp by remember {
+        mutableStateOf(1)
     }
+   Column(
+       modifier = Modifier
+           .fillMaxSize()
+           .background(Color.White),
+       horizontalAlignment = Alignment.CenterHorizontally,
+       verticalArrangement = Arrangement.Center
+   ) {
+       val diceImage = when(sideUp){
+           1 -> painterResource(id = R.drawable.dice_1)
+           2 -> painterResource(id = R.drawable.dice_2)
+           3 -> painterResource(id = R.drawable.dice_3)
+           4 -> painterResource(id = R.drawable.dice_4)
+           5 -> painterResource(id = R.drawable.dice_5)
+           else -> painterResource(id = R.drawable.dice_6)
+       }
+       Image(painter = diceImage, contentDescription = null)
+       Button(
+           onClick = {
+               sideUp = (1..6).random()
+           }
+       ) {
+           Text(text = "Roll the Dice")
+       }
+   } 
 }
 
-@Composable
-fun LemonTextAndImage(
-    textLabelResourceId: Int,
-    drawableResourceId: Int,
-    contentDescriptionResourceId: Int,
-    onImageClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Box(
-        modifier = modifier
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Button(
-                onClick = onImageClick,
-                shape = RoundedCornerShape(dimensionResource(R.dimen.button_corner_radius)),
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
-            ) {
-                Image(
-                    painter = painterResource(drawableResourceId),
-                    contentDescription = stringResource(contentDescriptionResourceId),
-                    modifier = Modifier
-                        .width(dimensionResource(R.dimen.button_image_width))
-                        .height(dimensionResource(R.dimen.button_image_height))
-                        .padding(dimensionResource(R.dimen.button_interior_padding))
-                )
-            }
-            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_vertical)))
-            Text(
-                text = stringResource(textLabelResourceId),
-                style = MaterialTheme.typography.bodyLarge
-            )
-        }
-    }
-}
 
 
 @Preview(showBackground = true)
 @Composable
 fun LemonadeAppPreview() {
     Android_basicsTheme {
-        LemonadeApp()
+        DiceRoller()
     }
 }
