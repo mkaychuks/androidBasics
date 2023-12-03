4716359075bf30569ec75650dc968b81fd080f4b
mkaychuks <sachufusi@gmail.com>
2023-12-03 20:20:54 +0100

    integration test example, honestly I don't understand it

--- app/src/androidTest/java/com/example/android_basics/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/android_basics/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.android_basics
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android_basics.ui.theme.Android_basicsTheme
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+import java.text.NumberFormat
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +26,26 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.android_basics", appContext.packageName)
+    }
+
+    @get: Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip(){
+        composeTestRule.setContent {
+            Android_basicsTheme {
+                TipCalculatorLayout()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage")
+            .performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found."
+        )
+
     }
 }
